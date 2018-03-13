88fd049e35999c04f0328690274f78996994fa3f
Arnaud Morin <arnaud.morin@gmail.com>
2018-03-13 11:31:29 +0100

    Add puppet et jump
    Signed-off-by: Arnaud Morin <arnaud.morin@gmail.com>

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function boot(){(
+    NAME=$1
+    IP=$2
+    openstack server create \
+        --key-name arnaud-ovh \
+        --nic net-id=Ext-Net \
+        --image 'Ubuntu 16.04' \
+        --flavor c2-7 \
+        --user-data userdata/${NAME/-[0-9]*/} \
+        $NAME
+)}
+
+boot jump
---
+++ puppet/manifests/site.pp
@@ -0,0 +1,3 @@
+node /^cat/ {
+  notify { 'salut': }
+}
